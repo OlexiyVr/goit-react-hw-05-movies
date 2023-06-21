26b1a26f45556a0b516c7ce02b8383c81a2b6bab
OlexiyVr <olexiyvr@gmail.com>
2023-06-21 20:29:17 +0300

    ms

---
+++ src/services/API.js
@@ -0,0 +1,70 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '559cbcab39764ce535027da10f396384';
+
+async function getTrendingMovies() {
+  const response = await fetch(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
+  );
+  if (response.ok) {
+    return await response.json();
+  }
+  return await Promise.reject(
+    new Error("Oops, something went wrong... We can't load trending movies :(")
+  );
+}
+
+async function getMovieById(id) {
+  const response = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  if (response.ok) {
+    return await response.json();
+  }
+  return await Promise.reject(
+    new Error(
+      'We are sorry, but we did not find any information about movie :('
+    )
+  );
+}
+
+async function getMovieCast(id) {
+  const response = await fetch(
+    `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`
+  );
+  if (response.ok) {
+    return await response.json();
+  }
+  return await Promise.reject(
+    new Error('We are sorry, but we did not find any information about cast :(')
+  );
+}
+
+async function getMovieReviews(id) {
+  const response = await fetch(
+    `${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}`
+  );
+  if (response.ok) {
+    return await response.json();
+  }
+  return await Promise.reject(
+    new Error('We do not have any reviews for this movie :(')
+  );
+}
+
+async function getMovieByQuery(query) {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+  );
+  if (response.ok) {
+    return await response.json();
+  }
+  return await Promise.reject(
+    new Error(`No results containing ${query} were found.`)
+  );
+}
+
+export {
+  getTrendingMovies,
+  getMovieById,
+  getMovieCast,
+  getMovieReviews,
+  getMovieByQuery,
+};
